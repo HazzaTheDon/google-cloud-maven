85b8ac58bcb1b5a1f7ad47260194dae061d08153
Your Name <you@example.com>
2020-02-03 12:19:36 +0000

    my second project

---
+++ src/main/java/com/qa/tutorial/Interaction.java
@@ -0,0 +1,19 @@
+package com.qa.tutorial;
+
+public class Interaction {
+	
+	/**
+	 * method for people to be greeted
+	 * @return a greeting
+	 */
+	public String greeting() {
+		return "Hi friend";
+
+
+	}
+
+	public String insult() {
+		return "Is trash";
+	}
+
+}
